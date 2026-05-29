9db8ae34c40b50ee9a7b2a8a2eb5abf20fab7381
Prithviraj-chw <prithvirajchowdhury06@gmail.com>
2026-05-29 17:04:30 +0530

    added ball bouncing logic and updated main with scoreboard

--- ball.py
+++ ball.py
@@ -5,8 +5,25 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("red")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_xcor = self.xcor() + 10
-        new_ycor = self.ycor() + 5
+        new_xcor = self.xcor() + self.x_move
+        new_ycor = self.ycor() + self.y_move
         self.goto(new_xcor,new_ycor)
+
+    def bounce_wall(self):
+        self.y_move *= -1 #reverses the direction of movement
+
+    def bounce_paddle(self):
+        self.x_move *= -1
+
+    def ball_reset(self):
+        self.goto(0, 0)
+        self.bounce_paddle() #ball resets and goes in the direction of the opposite player
+
+    def increase_speed(self):
+        self.x_move += 1
+        self.y_move += 1
+        self.move()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from turtle import Turtle,Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
+score = Scoreboard()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
 screen.title("Pong")
@@ -24,10 +26,32 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     ball.move()
-    
 
+    #detect collision with upper walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_wall()
 
+    #detect collision with paddles
+    if ball.xcor() > 330 and right_paddle.distance(ball) < 50:
+        """ .distance() checks the distance between the center of the paddle and the ball
+            so it glitches out when the ball hits the edge of the paddle"""
+        ball.bounce_paddle()
+        ball.increase_speed()
 
+    if ball.xcor() < -330 and left_paddle.distance(ball) < 50:
+        ball.bounce_paddle()
+        ball.increase_speed()
+
+    #detect if ball goes out of bounds
+    if ball.xcor() > 390:
+        ball.ball_reset()
+        score.clear()
+        score.l_point()
+
+    if ball.xcor() < -390:
+        ball.ball_reset()
+        score.clear()
+        score.r_point()
 
 
 
